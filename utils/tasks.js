3c6b0fe63d2294dae677cb30b516a6e5c32574f3
Nattapon Kumyaito <nattaponk@gmail.com>
2025-04-03 22:46:48 +0700

    log row object details in sendDataStore function

--- utils/tasks.js
+++ utils/tasks.js
@@ -56,6 +56,7 @@ async function sendDataStore() {
                 rowObj[cancode.dashboard_field] = totalAvg;
                 rowObj.device_type = cancode.device_type;
             }
+            console.log("Row Object:", rowObj);
             results.push(rowObj);
         }
 
